5ff38744517043af2676ddf65923e014a8ce3d5a
yashgajare <yashgajare16@gmail.com>
2025-07-20 01:52:07 +0530

    Added JWT and CORS and SecurityFilterChain config

--- src/main/java/com/backend/security/SecurityConfig.java
+++ src/main/java/com/backend/security/SecurityConfig.java
@@ -1,5 +1,59 @@
 package com.backend.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import com.backend.security.jwt.AuthEntryPointJwt;
+import com.backend.security.jwt.AuthTokenFilter;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
+	@Autowired
+	private AuthEntryPointJwt unauthorizedHandler;	
+	
+	@Bean
+	public AuthTokenFilter authenticationJwtTokenFilter() {
+		return new AuthTokenFilter();
+	}
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+		http
+		.csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+				.ignoringRequestMatchers("/api/auth/public/**"))
+		.cors(Customizer.withDefaults()) 
+        .httpBasic(Customizer.withDefaults())
+        .authorizeHttpRequests(authorize -> authorize
+        		.requestMatchers("/api/csrf-token").permitAll()
+        		.requestMatchers("/api/auth/public/**").permitAll()
+        		.anyRequest().authenticated())
+        .exceptionHandling(exception ->exception.authenticationEntryPoint(unauthorizedHandler))
+		.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+	
+		return http.build();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+	
 }
---
+++ src/main/java/com/backend/security/jwt/JwtUtils.java
@@ -0,0 +1,88 @@
+package com.backend.security.jwt;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+import javax.crypto.SecretKey;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.backend.services.impl.UserDetailsImpl;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+
+
+@Component
+public class JwtUtils {
+
+	private static final Logger lOGGER = LoggerFactory.getLogger(JwtUtils.class);
+	
+	@Value("${spring.app.jwtSecret}")
+	private String jwtSecret;
+	
+	@Value("${spring.app.jwtExpirationMs}")
+	private Integer jwtExpirationMs;
+	
+	public String getJwtFromHeader(HttpServletRequest request) {
+		String bearerToken = request.getHeader("Authorization");
+		lOGGER.debug("Authorization Header: {}", bearerToken);
+		if(bearerToken!=null && bearerToken.startsWith("Bearer ")) {
+			return bearerToken.substring(7);
+		}
+		return null;
+	}
+	
+	public String generateJwtFromUsername(UserDetailsImpl userDetails) {
+		String username = userDetails.getUsername();
+		String roles = userDetails.getAuthorities().stream().map(authority -> authority.getAuthority()).collect(Collectors.joining(","));
+		return Jwts
+				.builder()
+				.subject(username)
+				.claim("roles", roles)
+				.issuedAt(new Date())
+				.expiration(new Date(new Date().getTime() + jwtExpirationMs))
+				.signWith(key())
+				.compact();
+	}
+	
+	public String getUsernameFromJwtToken(String token) {
+		return Jwts
+				.parser()
+				.verifyWith((SecretKey) key())
+				.build()
+				.parseSignedClaims(token)
+				.getPayload()
+				.getSubject();
+	}
+	
+	public Key key() {
+		return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+	}
+	
+	public boolean validateJwtToken(String token) {
+		try {
+			System.out.println("Validate");
+			Jwts.parser().verifyWith((SecretKey) key()).build().parseSignedClaims(token);
+			return true;
+		} catch (MalformedJwtException e) {
+			lOGGER.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+        	lOGGER.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+        	lOGGER.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+        	lOGGER.error("JWT claims string is empty: {}", e.getMessage());
+        }
+		return false;
+	}
+}
